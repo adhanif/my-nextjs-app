38ed2e286472641306682ad166606577e042c5a0
adhanif <ad.hanif@outlook.com>
2025-07-16 17:07:39 +0300

    congired ci-cd eslint actions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,6 +9,26 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+// const eslintConfig = [...compat.extends("next/core-web-vitals", "prettier")];
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'prettier'],
+    rules: {
+      semi: ['error', 'always'],
+      'no-unused-vars': 'warn',
+      'import/no-unresolved': 'error', // Ensure imports are resolved correctly
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
+    },
+  }),
+];
 
 export default eslintConfig;
