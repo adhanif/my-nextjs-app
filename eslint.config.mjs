7805c5faed57de00e69e3deefef78a7b2b4ec14b
adhanif <ad.hanif@outlook.com>
2025-07-16 17:23:40 +0300

    configured eslint file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,19 +12,19 @@ const compat = new FlatCompat({
 // const eslintConfig = [...compat.extends("next/core-web-vitals", "prettier")];
 const eslintConfig = [
   ...compat.config({
-    extends: ['next/core-web-vitals', 'prettier'],
+    extends: ["next/core-web-vitals", "prettier"],
     rules: {
-      semi: ['error', 'always'],
-      'no-unused-vars': 'warn',
-      'import/no-unresolved': 'error', // Ensure imports are resolved correctly
-      'import/extensions': [
-        'error',
-        'ignorePackages',
+      semi: ["error", "always"],
+      "no-unused-vars": "error",
+      "import/no-unresolved": "error", // Ensure imports are resolved correctly
+      "import/extensions": [
+        "error",
+        "ignorePackages",
         {
-          js: 'never',
-          jsx: 'never',
-          ts: 'never',
-          tsx: 'never',
+          js: "never",
+          jsx: "never",
+          ts: "never",
+          tsx: "never",
         },
       ],
     },
